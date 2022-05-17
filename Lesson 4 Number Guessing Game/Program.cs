21646d8da1ebad92550e0122c3ad1eec893fae39
Gizzy <72341570+gzmerel@users.noreply.github.com>
2022-05-17 19:50:09 +1000

    the guess number is fixed.

--- Lesson 4 Number Guessing Game/Program.cs
+++ Lesson 4 Number Guessing Game/Program.cs
@@ -1,11 +1,12 @@
 ﻿// Week 4 Assignment Number Guessing Game 16/05/2022
 
 Console.WriteLine("Hello to Number Guessing Game!");
-var luckyNumber = new Random().Next(1,11);
+int luckyNumber = new Random().Next(1,11);
+Console.WriteLine(luckyNumber);
 int guessedNumber = 0;
 Console.WriteLine("Please guess a  number from 1 to 10, you have 3 guess ");
 Console.WriteLine("The game starts now: ");
-for (int  guess  = 3 ; 0<guess ; guess--)
+for (int  guess  = 2 ; 0<=guess ; guess--)
 {
     guessedNumber = int.Parse(Console.ReadLine().Trim());
     if (guessedNumber == luckyNumber)
@@ -15,14 +16,16 @@ for (int  guess  = 3 ; 0<guess ; guess--)
         }
     else if (guessedNumber > luckyNumber)
         {
-            Console.WriteLine($"{guessedNumber} higher than luckyNumber ");
-            Console.WriteLine($"you have {guess} guess left");     
+            Console.WriteLine($"{guessedNumber} is higher than luckyNumber ");
+            Console.WriteLine($"you have {guess} guess left");  
+            if (guess == 0) break;
             Console.WriteLine("Guess another number..." );
         }     
     else
         {
-            Console.WriteLine($"{guessedNumber} lower than luckyNumber ");
-            Console.WriteLine($"you have {guess} guess left");     
+            Console.WriteLine($"{guessedNumber} is lower than luckyNumber ");
+            Console.WriteLine($"you have {guess} guess left");   
+            if (guess == 0) break;
             Console.WriteLine("Guess another number..." );
         }
 } 

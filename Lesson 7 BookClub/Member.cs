4eb24cd0c9b7284941fb3954c2078f2c249a7b87
Gizzy <72341570+gzmerel@users.noreply.github.com>
2023-12-25 18:36:57 +1100

    Task 2 and task 3 added to the code
    Extension: (if you complete tasks 1 and 2, the extension task is to make this application interactive)
    Adapt the application so that it will accept commands from a user, and show the user what they want depending on what command they use. Allow the user to enter the commands
    Books - to list books recommended by the book club
    Members - to list the members
    Allow users to add new members
    Allow users to add new books to the recommended books list
    If you have done all this and want to try one more thing, add a command for the user to add new books to a member’s reading list.

--- Lesson 7 BookClub/Member.cs
+++ Lesson 7 BookClub/Member.cs
@@ -1,13 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace BookClub
 {
-public class Member
+    public class Member
     {
         public string firstName;
         public string lastName;
         public void PrintMember()
         {
-            Console.WriteLine($"{firstName} {lastName}'s favourite book is:{favouriteBook} ");
-            Console.WriteLine($"Books on {firstName}'s reading list");
+            Console.WriteLine($"{firstName} {lastName}'");
         }
         public string favouriteBook;
         public List<Book> readingList;
--- Lesson 7 BookClub/Program.cs
+++ Lesson 7 BookClub/Program.cs
@@ -1,64 +1,153 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System;
+using System.Collections;
+
 namespace BookClub
 {
     class Program
     {
         static void Main(string[] args)
         {
-        Console.WriteLine("Welcome to the library?");
-        var members = CreateMembers();
-        var recommendedBooks = CreateBookList();
-
-        foreach(var member in members)
-        {
-            member.PrintMember();
-        foreach (var book in recommendedBooks)
-            {
+            Console.WriteLine("What's your name?");
+            string Name = Console.ReadLine();
+            Console.WriteLine($"Hello {Name}");
+            Menu.DisplayMenu();
             
-                Book.PrintBook();
+            var members = CreateMembers();
+            var recommendedBooks = CreateBookList();
+
+            int userInput;
+
+            while (true)
+            {
+                Console.WriteLine("Enter your choice:");
+                userInput = int.Parse(Console.ReadLine().Trim());
+                switch (userInput)
+                {
+                    case 1:
+                        Console.WriteLine("Books recommemded by the book club");
+                        PrintBooks(recommendedBooks);
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Print List of Members");
+                        PrintMembers(members);
+                        break;
+
+                    case 3:
+                        Console.WriteLine("3.Add a new Member");
+                        AddNewMember(members);
+                        break;
+
+                    case 4:
+                        Console.WriteLine("Add new book to the new recommended book list");
+                        AddNewBook(recommendedBooks);
+                        break;
+                    case 0:
+                        Console.WriteLine("Exiting the program, GoodBye!");
+                        return;
+
+                    default:
+                        Console.WriteLine("Invalid input.Please enter a valid number.");
+                        break;
+                }
+                Menu.DisplayMenu();
+            }
+                
+            void PrintBooks(List<Book> books)
+            {
+                foreach ( var book in books)
+                {
+                    book.PrintBook();
+                }
+            }
+
+            void PrintMembers(List<Member> memberlist)
+            {
+                foreach ( var member in members)
+                {
+                    member.PrintMember();
+                }
+            }
+
+            
+            void AddNewMember(List<Member> member)
+            {
+                Console.WriteLine("Enter first name:");
+                string firstName = Console.ReadLine().Trim().ToString();
+                Console.WriteLine("Enter surname: ");
+                string lastName = Console.ReadLine().Trim().ToString();
+
+                member.Add(new Member
+                {
+                    firstName = firstName,
+                    lastName = lastName,
+                    readingList = new List<Book>()
+                });
+                Console.WriteLine("New member successfully added");
+
+            }
+            void AddNewBook(List<Book> book)
+            {
+                Console.WriteLine("Enter title:");
+                string title = Console.ReadLine().Trim().ToString(); ;
+                Console.WriteLine("Enter author");
+                string author = Console.ReadLine().Trim().ToString(); ;
+                
+                book.Add(new Book { 
+                    title = title,
+                    author = author 
+                });
+                Console.WriteLine("New book successfully added");
+
+            }
+
+
+                List<Member> CreateMembers()
+                {
+                    var person1 = new Member
+                    {
+                        firstName = "Jane",
+                        lastName = "Doe",
+                        readingList = new List<Book> { new Book() }
+                    };
+                    var person2 = new Member
+                    {
+                        firstName = "Gary",
+                        lastName = "Brown",
+                        readingList = new List<Book> { new Book() }
+                    };
+
+                    var member = new List<Member>
+                {
+                    person1 , person2,
+                };
+
+                return member;
+            }
+
+                List<Book> CreateBookList()
+                {
+                    var book1 = new Book
+                    {
+                        title = "atomic habits",
+                        author = "James Clear",
+
+                    };
+                    var book2 = new Book
+                    {
+                        title = "The Museum of Innocence",
+                        author = "Orhan Pamuk",
+
+                    };
+
+                    var books = new List<Book>{
+                            book1 , book2,
+                    };
+
+                return books;
+            }
+
             }
         }
-    
-
-    static List<Member> CreateMembers()
-    {
-        var person1 = new Member{
-            firstName = "Jane",
-            lastName = "Doe",
-            readingList = new List<Book> { new Book() }
-        };
-        var person2 = new Member{
-            firstName = "Gary",
-            lastName = "Brown",
-            readingList = new List<Book> { new Book() }
-        };
-
-        var members = new List<Member>{ 
-            person1 , person2,
-        };
-
-        return members;
-    }
-    static List<Book> CreateBookList()
-    {
-        var book1 = new Book{
-            title = "atomic habits",
-            author = "James Clear",
-            
-        };
-        var book2 = new Book{
-            title = "The Museum of Innocence",
-            author = "Orhan Pamuk",
-            
-        };
-
-        var books = new List<Book>{ 
-            book1 , book2,
-        };
-
-        return books;
-    }
-
-        }
-    }
 }

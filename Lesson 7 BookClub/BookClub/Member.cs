d11f4421b1170140d14f82186f62e783e9abd11e
Gizzy <72341570+gzmerel@users.noreply.github.com>
2023-12-25 16:26:17 +1100

    Updated version in SV 2022

---
+++ Lesson 7 BookClub/BookClub/Member.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookClub
+{
+    public class Member
+    {
+        public string firstName;
+        public string lastName;
+        public void PrintMember()
+        {
+            Console.WriteLine($"{firstName} {lastName}'s favourite book is:{favouriteBook} ");
+            Console.WriteLine($"Books on {firstName}'s reading list");
+        }
+        public string favouriteBook;
+        public List<Book> readingList;
+    }
+}
---
+++ Lesson 7 BookClub/BookClub/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace BookClub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the library!");
+            var members = CreateMembers();
+            var recommendedBooks = CreateBookList();
+
+            foreach (var member in members)
+            {
+                member.PrintMember();
+
+                foreach (var book in recommendedBooks)
+                {
+                    book.PrintBook(); 
+                }
+            }
+
+
+             List<Member> CreateMembers()
+            {
+                var person1 = new Member
+                {
+                    firstName = "Jane",
+                    lastName = "Doe",
+                    readingList = new List<Book> { new Book() }
+                };
+                var person2 = new Member
+                {
+                    firstName = "Gary",
+                    lastName = "Brown",
+                    readingList = new List<Book> { new Book() }
+                };
+
+                var member = new List<Member>
+                {
+                    person1 , person2,
+                };
+
+                return member;
+            }
+
+             List<Book> CreateBookList()
+            {
+                var book1 = new Book
+                {
+                    title = "atomic habits",
+                    author = "James Clear",
+
+                };
+                var book2 = new Book
+                {
+                    title = "The Museum of Innocence",
+                    author = "Orhan Pamuk",
+
+                };
+
+                var books = new List<Book>{
+            book1 , book2,
+        };
+
+                return books;
+            }
+
+        }
+    }
+}
